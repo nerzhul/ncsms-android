f78c4f2fb2ce048352013ad5315d42bb51003695
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-10-10 16:16:53 +0200

    SmsDataProvider is under AGPLv3 instead of BSD. Import some code. Import some SmsFetcher code into SmsDataProvider. AGPLv3 is better for next published code. I think this will be the licence for many parts of R&D code, but BSD will remain on autogenerated code, layouts and strings

--- src/fr/unix_experience/owncloud_sms/providers/SmsDataProvider.java
+++ src/fr/unix_experience/owncloud_sms/providers/SmsDataProvider.java
@@ -1,45 +1,55 @@
 package fr.unix_experience.owncloud_sms.providers;
 
 /*
- * Copyright (c) 2014, Loic Blot <loic.blot@unix-experience.fr>
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ *  Copyright (c) 2014, Loic Blot <loic.blot@unix-experience.fr>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 
 public class SmsDataProvider extends ContentProvider {
+	public SmsDataProvider () {}
+	
+	public SmsDataProvider (Context ct) {
+		super();
+		_context = ct;
+	}
 
 	@Override
 	public boolean onCreate() {
 		return false;
 	}
 
+	public Cursor query(String mailBox) {
+		return query(Uri.parse(mailBox),
+			new String[] { "read", "date", "address", "seen", "body", "_id", "type", },
+			null, null, null
+		);
+	}
+
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
+		if (_context != null && _context.getContentResolver() != null) {
+			return _context.getContentResolver().query(uri, projection, selection, selectionArgs, sortOrder);
+		}
+		
 		return null;
 	}
 
@@ -68,4 +78,5 @@ public class SmsDataProvider extends ContentProvider {
 		return 0;
 	}
 
+	private Context _context;
 }
