a8db6e9b9e16032cd330d4f80d2f84d59de27263
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-10-22 07:56:08 +0200

    Added Notification handlers as AGPLv3

---
+++ src/fr/unix_experience/owncloud_sms/notifications/OCSMSNotificationManager.java
@@ -0,0 +1,56 @@
+package fr.unix_experience.owncloud_sms.notifications;
+
+/*
+ *  Copyright (c) 2014, Loic Blot <loic.blot@unix-experience.fr>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import fr.unix_experience.owncloud_sms.R;
+import fr.unix_experience.owncloud_sms.enums.OCSMSNotificationType;
+import android.content.Context;
+
+public class OCSMSNotificationManager {
+
+	public OCSMSNotificationManager(Context context) {
+		_context = context;
+		_notification = new OCSMSNotification(_context);
+	}
+
+	public void setSyncProcessMsg() {
+		createNotificationIfPossible(OCSMSNotificationType.SYNC, 
+			_context.getString(R.string.sync_title), 
+			_context.getString(R.string.sync_inprogress)
+		);
+	}
+	
+	public void dropSyncProcessMsg() {
+		_notification.cancelNotify(OCSMSNotificationType.SYNC);
+	}
+	
+	public void setSyncErrorMsg(String errMsg) {
+		createNotificationIfPossible(OCSMSNotificationType.SYNC_FAILED, 
+			_context.getString(R.string.sync_title), 
+			_context.getString(R.string.fatal_error) + "\n" + errMsg
+		);
+	}
+	
+	private void createNotificationIfPossible(OCSMSNotificationType nType, String nTitle, String nMsg) {
+		_notification.createNotify(nType, nTitle, nMsg);
+	}
+	
+	private Context _context;
+	private OCSMSNotification _notification;
+	
+}
