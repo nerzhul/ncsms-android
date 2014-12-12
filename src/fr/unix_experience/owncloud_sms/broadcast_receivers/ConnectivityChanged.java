6292011e9bd8aaaf6eeb4b587360aec253e2c33a
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-12-12 15:22:02 +0100

    Event: connectivity changed: prepare a handler when data conn become available

--- src/fr/unix_experience/owncloud_sms/broadcast_receivers/ConnectivityChanged.java
+++ src/fr/unix_experience/owncloud_sms/broadcast_receivers/ConnectivityChanged.java
@@ -1,18 +1,34 @@
 package fr.unix_experience.owncloud_sms.broadcast_receivers;
 
-import fr.unix_experience.owncloud_sms.notifications.OCSMSNotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 public class ConnectivityChanged extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		Log.d(TAG,"ConnectivityChanged.onReceive");
-		(new OCSMSNotificationManager(context)).setDebugMsg("ConnectivityChanged");
+	public void onReceive(Context context, Intent intent) {		
+		// Check the connectivity
+		final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		final android.net.NetworkInfo niWiFi = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		final android.net.NetworkInfo niMobile = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		
+		// If data is available and previous dataConnectionState was false, then we need to sync
+		if ((niWiFi.isAvailable() || niMobile.isAvailable()) && dataConnectionAvailable == false) {
+			dataConnectionAvailable = true;
+			Log.d(TAG,"ConnectivityChanged.onReceive, data conn available");
+			// @TODO: check if last message is last synced msg (shared preference)
+		}
+		// No data available and previous dataConnectionState was true
+		else if (dataConnectionAvailable == true && !niWiFi.isAvailable() && !niMobile.isAvailable()) {
+			dataConnectionAvailable = false;
+		}
 	}
+	
+	private static boolean dataConnectionAvailable = false;
 	
 	private static final String TAG = ConnectivityChanged.class.getSimpleName();
 }
