f588a1ccb101731033c9e4e3be5a7e0db0494d76
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-12-12 15:52:34 +0100

    Forget copyright

--- src/fr/unix_experience/owncloud_sms/prefs/OCSMSSharedPrefs.java
+++ src/fr/unix_experience/owncloud_sms/prefs/OCSMSSharedPrefs.java
@@ -1,5 +1,22 @@
 package fr.unix_experience.owncloud_sms.prefs;
 
+/*
+ *  Copyright (c) 2014, Loic Blot <loic.blot@unix-experience.fr>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import fr.unix_experience.owncloud_sms.R;
 import android.content.Context;
 import android.content.SharedPreferences;
